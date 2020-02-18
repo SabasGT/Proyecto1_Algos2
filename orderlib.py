734c210028f8d34cd1a5cfb21b075a11b382d377
Sabas Gonzalez <sabasgt98@gmail.com>
2020-02-17 21:34:01 -0400

    Update orderlib.py
    Añadido Quicksort Median

--- orderlib.py
+++ orderlib.py
@@ -1,4 +1,5 @@
 import numpy as np
+from statistics import median
 
 def insertionSort(A:list) -> "list":
 	# assert(len(A)>0)                # Precondicion
@@ -105,10 +106,87 @@ def partition(A:list, p:int, r:int) -> int:
 
 def quickSort(A:list, p:int, r:int) -> "void":
 	# Correcta inicializacion con p = 0 y r = len(A) - 1
-	if p + r + 1 <= 100:
+	if p + r + 1 <= 32:
 		insertionSortIndex(A, p, r)
 	else:
 		if p < r:
 			q = partition(A, p, r)
 			quickSort(A,p,q-1)
 			quickSort(A,q,r)
+
+
+def right(i:int) -> int:
+	k = 2*i + 1
+	return k
+
+
+def left(i:int) -> int:
+	k = 2*i
+	return k
+
+
+def max_heapify(A:list, i:int, n:int) -> "void":
+	l = left(i)
+	r = right(i)
+	if l < n and A[l] > A[i]:
+		largest = l
+	else:
+		largest = i
+
+	if r < n and A[r]> A[largest]:
+		largest = r
+
+	if largest != i:
+		A[i], A[largest] = A[largest], A[i]
+		max_heapify(A, largest, n)
+
+
+def build_max_heap(A:list) -> "void":
+	n = len(A)
+
+	for i in range(n//2, -1, -1):
+		max_heapify(A, i, n)
+
+
+def heapSort(A:list) -> "void":
+	n = len(A)
+	build_max_heap(A)
+
+	for i in range(n - 1, 0,-1):
+		A[0], A[i] = A[i], A[0]
+		max_heapify(A,0, i)
+
+
+def partitionLoop(A:list, p:int, r:int, x:int) -> "int":
+	i = p - 1
+	j = r
+	while True:
+		while A[j] > x:
+			j = j - 1
+
+		while A[i] < x:
+			i = i + 1
+
+		if i < j:
+			A[i], A[j] = A[j], A[i]
+		else:
+			return j
+
+
+
+def quicksortLoop(A:list, f:int, b:int) -> "void":
+	while b - f > 32:
+		p = partitionLoop(A, f, b, median([A[f], A[f + ((b - f)//2)], A[b - 1]]))
+		if (p - f) >= (b - p):
+			quicksortLoop(A, p, b)
+			b = p
+		else:
+			quicksortLoop(A, f, p)
+			f = p
+
+
+
+def quicksortMedian(A:list, f:int, b:int) -> "void":
+	# b es la posicion mas alla del final de la secuencia, osea, es len(A) puro.
+	quicksortLoop(A, f, b)
+	insertionSortIndex(A, f, b)
